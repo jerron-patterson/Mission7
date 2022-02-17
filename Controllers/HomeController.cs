fe43a802693f395a87e727de719e9fb32d2f0277
Jerron <jerron.byu@gmail.com>
2022-02-16 22:44:21 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission7.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*        private readonly ILogger<HomeController> _logger;
+
+                public HomeController(ILogger<HomeController> logger)
+                {
+                    _logger = logger;
+                }*/
+
+
+        private IBookStoreRepository repo;
+        public HomeController (IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pgSz = 10;
+
+            var context = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pgSz)
+                .Take(pgSz);
+            return View(context);
+        }
+    }
+}
---
+++ Models/EFBookStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+    public class EFBookStoreRepository : IBookStoreRepository
+    {
+        private BookstoreContext context { get; set; }
+        public EFBookStoreRepository (BookstoreContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
