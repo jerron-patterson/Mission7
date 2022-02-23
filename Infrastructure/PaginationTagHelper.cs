6f7efd9f6d9a4cd4e6eb7dd1d9f2ea08eaafe43e
Jerron <jerron.byu@gmail.com>
2022-02-23 16:50:34 -0700

    update

--- Infrastructure/PaginationTagHelper.cs
+++ Infrastructure/PaginationTagHelper.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Mission7.Infrastructure
 {
-    public class PaginationTagHelper
+    [HtmlTargetElement("div", Attributes = "pg-mdl")]
+    public class PaginationTagHelper : TagHelper
     {
     }
 }
---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TtlNumBooks { get; set; }
+        public int BooksPerPg { get; set; }
+        public int CurrentPg { get; set; }
+        public int TtlPgs => (int) Math.Ceiling((double) TtlNumBooks / BooksPerPg);
+    }
+}
